d8b2bf28a2736c67a0c49a0eba056ce75f2609ea
asadi <foraliasadi@gmail.com>
2025-06-02 23:58:25 +0330

    initial

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Grid templateAreas={`"nav nav" "aside main"`}>
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <GridItem area="aside" bg="gold">
+        aside
+      </GridItem>
+      <GridItem area="main" bg="dodgerblue">
+        main
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
