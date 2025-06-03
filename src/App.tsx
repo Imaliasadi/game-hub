371f88d80e87300d13cd932df4bf1c5738db0c8a
asadi <foraliasadi@gmail.com>
2025-06-03 23:42:23 +0330

    made a color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Grid templateAreas={`"nav nav" "aside main"`}>
-      <GridItem area="nav" bg="coral">
-        Nav
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
       </GridItem>
-      <GridItem area="aside" bg="gold">
+      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
         aside
       </GridItem>
       <GridItem area="main" bg="dodgerblue">
---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,20 @@
+import { HStack, Text } from "@chakra-ui/react";
+import { useColorMode } from "./ui/color-mode";
+import { Switch } from "./ui/switch";
+
+function ColorModeSwitch() {
+  const { toggleColorMode, colorMode } = useColorMode();
+  return (
+    <HStack>
+      <Switch
+        colorPalette={"green"}
+        checked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      >
+        <Text>Dark Mode</Text>
+      </Switch>
+    </HStack>
+  );
+}
+
+export default ColorModeSwitch;
