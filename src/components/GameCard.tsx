96ab013c9b2edf0b15be26d4163fc70fa7ecc4f4
asadi <foraliasadi@gmail.com>
2025-06-11 16:47:13 +0330

    show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 import type { Game } from "@/hooks/useGames";
 import { Card, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
-import CriticScore from "./criticScore";
+import CriticScore from "./CriticScore";
+import OptimiseImageSize from "@/services/image-url";
 
 interface Props {
   game: Game;
@@ -9,9 +10,9 @@ interface Props {
 
 function GameCard({ game }: Props) {
   return (
-    <Card.Root borderRadius={10} overflow="hidden">
+    <Card.Root borderRadius={10} overflow="hidden" width={"300px"}>
       <Card.Body>
-        <Image src={game.background_image} />
+        <Image src={OptimiseImageSize(game.background_image)} />
         <Card.Title fontSize="2xl">{game.name}</Card.Title>
         <HStack justifyContent="space-between">
           <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -1,12 +1,17 @@
 import useGames from "@/hooks/useGames";
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 function GamesGrid() {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
       {error && <Text>{error}</Text>}
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap={10}>
+        {isLoading && skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+      </SimpleGrid>
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} gap={10}>
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
