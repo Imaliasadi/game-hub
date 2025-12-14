22d7c41e970a3e58e30e92eecdcf5639bf883ac4
Ali <foraliasadi@gmail.com>
2025-12-14 21:14:24 +0330

    setup routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import { Provider } from "@/components/ui/provider";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router-dom";
+import router from "./routs";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools initialIsOpen={false} />
       </Provider>
     </QueryClientProvider>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,25 +1,21 @@
+import GameGenres from "@/components/GameGenres";
+import GamesGrid from "@/components/GamesGrid";
+import DynamicHeading from "@/components/Heading";
+import PlatformSlector from "@/components/PlatformSlector";
+import SortSelector from "@/components/SortSelector";
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Analytics } from "@vercel/analytics/react";
-import GameGenres from "./components/GameGenres";
-import GamesGrid from "./components/GamesGrid";
-import DynamicHeading from "./components/Heading";
-import NavBar from "./components/NavBar";
-import PlatformSlector from "./components/PlatformSlector";
-import SortSelector from "./components/SortSelector";
 
-function App() {
+function HomePage() {
   return (
     <>
       <Grid
         padding={3}
         templateAreas={{
-          base: `"nav" "main"`,
-          lg: `"nav nav" "aside main"`,
+          base: `"main"`,
+          lg: `"aside main"`,
         }}
       >
-        <GridItem area="nav">
-          <NavBar />
-        </GridItem>
         <GridItem area="aside" display={{ base: "none", lg: "block" }} paddingX={"5"}>
           <GameGenres />
         </GridItem>
@@ -35,4 +31,4 @@ function App() {
   );
 }
 
-export default App;
+export default HomePage;
